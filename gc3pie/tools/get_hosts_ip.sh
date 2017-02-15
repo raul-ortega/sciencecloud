140ce30ce2c79206551d24522c13aeaa83c4eb7b
Raúl Ortega <raul-ortega@users.noreply.github.com>
2017-02-15 16:37:38 +0100

    tool added

---
+++ gc3pie/tools/get_hosts_ip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+jobs=$(ls $1/jobs)
+for job in $jobs
+do
+	ip=""
+	id=""
+
+	id=$(ginfo -s $1 $job -v 2> /dev/null | grep "_lrms_vm_id: " | awk -F' ' '{print $2}')
+	if [ "$id" = "" ];then
+		echo -e $job "\tvms not found"
+	else
+		ip=$(gcloud list 2> /dev/null | grep "$id" | sed 's/ | /,/g' | awk -F',' '{print $4}')
+		echo -e $job"\t"b$id"\t"$ip
+	fi
+done
